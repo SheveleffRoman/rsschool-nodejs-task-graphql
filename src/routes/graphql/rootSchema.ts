0848fdce4c76d922a48205e0ad95f9c9007df260
SheveleffRoman <@>
2024-03-04 22:18:10 +0400

    feat: add mutation types to root schema

--- src/routes/graphql/rootSchema.ts
+++ src/routes/graphql/rootSchema.ts
@@ -1,8 +1,18 @@
-import { GraphQLObjectType, GraphQLNonNull, GraphQLList, GraphQLSchema } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
 import { memberType, memberTypeIdEnum } from './types/memberTypes.js';
-import { postType } from './types/postTypes.js';
-import { profileType } from './types/profileTypes.js';
-import { userType } from './types/userTypes.js';
+import { changePostInputType, createPostInputType, postType } from './types/postTypes.js';
+import {
+  changeProfileInputType,
+  createProfileInputType,
+  profileType,
+} from './types/profileTypes.js';
+import { changeUserInputType, createUserInputType, userType } from './types/userTypes.js';
 import { UUIDType } from './types/uuidTypes.js';
 
 export const MyAppQueryRootType = new GraphQLObjectType({
@@ -49,7 +59,79 @@ export const MyAppQueryRootType = new GraphQLObjectType({
 
 export const MyAppMutationRootType = new GraphQLObjectType({
   name: 'Mutation',
-  fields: {},
+  fields: {
+    createUser: {
+      type: userType,
+      args: {
+        dto: { type: createUserInputType },
+      },
+    },
+    changeUser: {
+      type: userType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: changeUserInputType },
+      },
+    },
+    deleteUser: {
+      type: UUIDType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    createPost: {
+      type: postType,
+      args: {
+        dto: { type: createPostInputType },
+      },
+    },
+    changePost: {
+      type: postType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: changePostInputType },
+      },
+    },
+    deletePost: {
+      type: UUIDType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    createProfile: {
+      type: profileType,
+      args: {
+        dto: { type: createProfileInputType },
+      },
+    },
+    changeProfile: {
+      type: profileType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: changeProfileInputType },
+      },
+    },
+    deleteProfile: {
+      type: UUIDType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    subscribeTo: {
+      type: userType,
+      args: {
+        userId: { type: new GraphQLNonNull(UUIDType) },
+        authorId: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    unsubscribeFrom: {
+      type: GraphQLString,
+      args: {
+        userId: { type: new GraphQLNonNull(UUIDType) },
+        authorId: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+  },
 });
 
 export const MyAppSchema = new GraphQLSchema({
