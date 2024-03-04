0f6adb00524ee3e65815c8366973d3922950828c
SheveleffRoman <@>
2024-03-04 23:13:31 +0400

    feat: add user mutation handlers

--- src/routes/graphql/handlers/userHandlers.ts
+++ src/routes/graphql/handlers/userHandlers.ts
@@ -5,16 +5,26 @@ import {
 } from 'graphql-parse-resolve-info';
 
 import { GraphQLList, GraphQLResolveInfo } from 'graphql';
-import { MissedArgs, PrismaAppData } from '../interfaces/app.interface.js';
+import {
+  MissedArgs,
+  PrismaAppData,
+  SubscriptionMutationInput,
+} from '../interfaces/app.interface.js';
 import { userType } from '../types/userTypes.js';
+import { User } from '@prisma/client';
 
 export const userHandlers = {
   user: getUser,
   users: getUsers,
+  createUser: createUser,
+  changeUser: changeUser,
+  deleteUser: deleteUser,
+  subscribeTo: subscribeTo,
+  unsubscribeFrom: unsubscribeFrom,
 };
 
 async function getUser({ id }: { id: string }, { userLoader }: PrismaAppData) {
-  return await userLoader.load(id);
+  return userLoader.load(id);
 }
 
 async function getUsers(
@@ -41,4 +51,58 @@ async function getUsers(
   });
 
   return users;
+}
+
+async function createUser({ dto: data }: { dto: User }, { prisma }: PrismaAppData) {
+  return prisma.user.create({ data });
+}
+
+async function changeUser(
+  { id, dto: data }: { id: string } & { dto: Partial<User> },
+  { prisma }: PrismaAppData,
+) {
+  try {
+    return await prisma.user.update({
+      where: { id },
+      data,
+    });
+  } catch {
+    return null;
+  }
+}
+
+async function deleteUser({ id }: { id: string }, { prisma }: PrismaAppData) {
+  try {
+    await prisma.user.delete({ where: { id } });
+    return id;
+  } catch {
+    return null;
+  }
+}
+
+async function subscribeTo(
+  { userId: id, authorId }: SubscriptionMutationInput,
+  { prisma }: PrismaAppData,
+) {
+  try {
+    return prisma.user.update({
+      where: { id },
+      data: { userSubscribedTo: { create: { authorId } } },
+    });
+  } catch {
+    return null;
+  }
+}
+
+async function unsubscribeFrom(
+  { userId: subscriberId, authorId }: SubscriptionMutationInput,
+  { prisma }: PrismaAppData,
+) {
+  try {
+    await prisma.subscribersOnAuthors.delete({
+      where: { subscriberId_authorId: { subscriberId, authorId } },
+    });
+  } catch {
+    return null;
+  }
 }
