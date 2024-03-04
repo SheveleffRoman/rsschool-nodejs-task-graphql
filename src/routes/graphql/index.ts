9a03881cdab288aafef7c1a48d5d9f1a5e78c91f
SheveleffRoman <@>
2024-03-04 20:53:39 +0400

    feat: add graphql initialization, limit, validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,14 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+import { getAllRestHandlers } from './handlers/handlers.js';
+import { getDataLoaders } from './loaders/getDataLoaders.js';
+import { MyAppSchema } from './rootSchema.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,7 +19,19 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+      const errors = validate(MyAppSchema, parse(query), [depthLimit(5)]);
+      if (errors.length) {
+        return { errors };
+      }
+      const response = await graphql({
+        schema: MyAppSchema,
+        source: query,
+        rootValue: getAllRestHandlers(),
+        variableValues: variables,
+        contextValue: { prisma: prisma, ...getDataLoaders(prisma) },
+      });
+      return response;
     },
   });
 };
