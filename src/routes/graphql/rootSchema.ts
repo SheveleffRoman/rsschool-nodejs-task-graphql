5a42d7e10d445bbb4bf5d7d90cab13d5f4e66a11
SheveleffRoman <@>
2024-03-04 17:09:19 +0400

    feat: add query fields

--- src/routes/graphql/rootSchema.ts
+++ src/routes/graphql/rootSchema.ts
@@ -1,8 +1,50 @@
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { GraphQLObjectType, GraphQLNonNull, GraphQLList, GraphQLSchema } from 'graphql';
+import { memberType, memberTypeIdEnum } from './types/memberTypes.js';
+import { postType } from './types/postTypes.js';
+import { profileType } from './types/profileTypes.js';
+import { userType } from './types/userTypes.js';
+import { UUIDType } from './types/uuidTypes.js';
 
 export const MyAppQueryRootType = new GraphQLObjectType({
   name: 'Query',
-  fields: {},
+  fields: {
+    user: {
+      type: userType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    users: {
+      type: new GraphQLList(userType),
+    },
+    memberType: {
+      type: memberType,
+      args: {
+        id: { type: new GraphQLNonNull(memberTypeIdEnum) },
+      },
+    },
+    memberTypes: {
+      type: new GraphQLList(memberType),
+    },
+    post: {
+      type: postType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    posts: {
+      type: new GraphQLList(postType),
+    },
+    profile: {
+      type: profileType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    profiles: {
+      type: new GraphQLList(profileType),
+    },
+  },
 });
 
 export const MyAppMutationRootType = new GraphQLObjectType({
