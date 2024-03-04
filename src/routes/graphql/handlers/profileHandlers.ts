34567f7ac7592c5b8689e9bf5bbd5443131f4ff4
SheveleffRoman <@>
2024-03-04 22:59:33 +0400

    feat: add profile mutation handlers

--- src/routes/graphql/handlers/profileHandlers.ts
+++ src/routes/graphql/handlers/profileHandlers.ts
@@ -1,8 +1,12 @@
+import { Profile } from '@prisma/client';
 import { PrismaAppData, MissedArgs } from '../interfaces/app.interface.js';
 
 export const profileHandlers = {
   profile: getProfile,
   profiles: getProfiles,
+  createProfile: createProfile,
+  changeProfile: changeProfile,
+  deleteProfile: deleteProfile,
 };
 
 async function getProfile({ id }: { id: string }, { prisma }: PrismaAppData) {
@@ -11,4 +15,35 @@ async function getProfile({ id }: { id: string }, { prisma }: PrismaAppData) {
 
 async function getProfiles(_: MissedArgs, { prisma }: PrismaAppData) {
   return await prisma.profile.findMany();
+}
+
+async function createProfile({ dto: data }: { dto: Profile }, { prisma }: PrismaAppData) {
+  try {
+    return await prisma.profile.create({ data });
+  } catch {
+    return null;
+  }
+}
+
+async function changeProfile(
+  { id, dto: data }: { id: string } & { dto: Partial<Profile> },
+  { prisma }: PrismaAppData,
+) {
+  try {
+    return await prisma.profile.update({
+      where: { id },
+      data,
+    });
+  } catch {
+    return null;
+  }
+}
+
+async function deleteProfile({ id }: { id: string }, { prisma }: PrismaAppData) {
+  try {
+    await prisma.profile.delete({ where: { id } });
+    return id;
+  } catch {
+    return null;
+  }
 }
