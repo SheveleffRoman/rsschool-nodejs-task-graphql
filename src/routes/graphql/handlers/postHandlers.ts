b16be0cbfbfeea34c52d80eeea10616d058d791c
SheveleffRoman <@>
2024-03-04 22:57:56 +0400

    feat: add mutaion post handlers

--- src/routes/graphql/handlers/postHandlers.ts
+++ src/routes/graphql/handlers/postHandlers.ts
@@ -1,8 +1,12 @@
+import { Post } from '@prisma/client';
 import { PrismaAppData, MissedArgs } from '../interfaces/app.interface.js';
 
 export const postHandlers = {
   post: getPost,
   posts: getPosts,
+  createPost: createPost,
+  changePost: changePost,
+  deletePost: deletePost,
 };
 
 async function getPost({ id }: { id: string }, { prisma }: PrismaAppData) {
@@ -13,4 +17,33 @@ async function getPost({ id }: { id: string }, { prisma }: PrismaAppData) {
 async function getPosts(_: MissedArgs, { prisma }: PrismaAppData) {
   const posts = await prisma.post.findMany();
   return posts;
+}
+
+async function createPost({ dto: data }: { dto: Post }, { prisma }: PrismaAppData) {
+  const post = await prisma.post.create({ data });
+  return post;
+}
+
+async function changePost(
+  { id, dto: data }: { id: string } & { dto: Partial<Post> },
+  { prisma }: PrismaAppData,
+) {
+  try {
+    const post = await prisma.post.update({
+      where: { id },
+      data,
+    });
+    return post;
+  } catch {
+    return null;
+  }
+}
+
+async function deletePost({ id }: { id: string }, { prisma }: PrismaAppData) {
+  try {
+    await prisma.post.delete({ where: { id } });
+    return id;
+  } catch {
+    return null;
+  }
 }
